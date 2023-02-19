fe43f974bac15dffac72a241082ce31587e7e1e2
Sutinder S. Saini <sutinder456@gmail.com>
2023-02-19 14:22:09 -0800

    changes so i dont get docked points

--- HO3/HO3C.asm
+++ HO3/HO3C.asm
@@ -28,194 +28,156 @@ month	    DWORD   ?
 day	    DWORD   ?
 age	    DWORD   ?
 
-; Start 
-; curYear = 2019 
-; curMonth = 10 
-; curDay = 20 
-; Print "Enter the year of your birth: " 
-; Input year 
-; IF year = curYear THEN 
-;   Print "you are less than 1 year old" 
-; ELSE 
-;   IF year < curYear THEN
-;     Print "Enter the month of your birth: " ++++
-;     Input month 
-;     age = curYear - year 
-;     IF month > curMonth THEN  
-;       age = age - 1 
-;       Print "you are "; age; " years old" 
-;     ELSE
-;       IF month < curMonth THEN 
-;         Print "you are "; age; " years old" 
-;       ELSE 
-;         Print "Enter day of your birth: "
-;         Input day 
-;         IF day > curDay THEN 
-;           age = age - 1 
-;           Print "you are "; age; " years old" 
-;         ELSE 
-;           IF day < curDay THEN 
-;             Print "you are "; age; " years old" 
-;           ELSE 
-;             Print "you are "; age; " years old, Happy Birthday" 
-;           ENDIF 
-;         ENDIF 
-;       ENDIF 
-;     ENDIF 
-;   ELSE 
-;     Print "you haven't been born year" 
-;   ENDIF 
-; ENDIF 
-; Stop 
-
 .code
 main	    PROC
     
-    mov	    edx, OFFSET	yearMsg
-    call    WriteString
+	    mov	    edx, OFFSET	yearMsg
+	    call    WriteString
 
-    call    ReadDec
-    mov	    year, EAX
+	    call    ReadDec
+	    mov	    year, EAX
 
-    mov	    EAX, year
-    cmp	    EAX, curYear
-    je	    IF_CURRENT_YEAR
+	    mov	    EAX, year
+	    cmp	    EAX, curYear		    ; IF year < curYear
+	    je	    IF_CURRENT_YEAR		    ; THEN
 
-    jg	    PERSON_HAS_NOT_BEEN_BORN_YET
-
-    jmp	    IF_ONE
+	    jg	    PERSON_HAS_NOT_BEEN_BORN_YET    ; IF year > curYear
+	    jmp	    IF_ONE			    ; ELSE
 
 IF_ONE:
-    call    subYearFromCurYear
-    mov	    edx, OFFSET monthMsg
-    call    WriteString
+	    call    subYearFromCurYear
+	    mov	    edx, OFFSET monthMsg
+	    call    WriteString
 
-    call    ReadDec
-    mov	    month, eax
+	    call    ReadDec
+	    mov	    month, eax
 
-    cmp	    eax, curMonth
+	    cmp	    eax, curMonth
     
-    jg	    MONTH_GREATER_THAN_CURRENT_MONTH			;; is x less than y ?
+	    jg	    MONTH_GREATER_THAN_CURRENT_MONTH
 
-    jl	    MONTH_LESS_THAN_CURRENT_MONTH		        ;; is y less than x ?
+	    jl	    MONTH_LESS_THAN_CURRENT_MONTH
     
-    jmp	    INNER_ELSE_3					;; more clarification needed
-    ret
+	    jmp	    INNER_ELSE_3
+	    jmp	    _ENDIF			    ; ENDIF
 
 MONTH_GREATER_THAN_CURRENT_MONTH: 
-    mov	    edx, OFFSET youAreMsg
-    call    WriteString
+	    mov	    edx, OFFSET youAreMsg
+	    call    WriteString
     
-    call    decrementAge
-    call    WriteDec
+	    call    decrementAge
+	    call    WriteDec
 
-    mov	    edx, OFFSET yearsOldMsg
-    call    WriteString
-    ret
+	    mov	    edx, OFFSET yearsOldMsg
+	    call    WriteString
+	    jmp	    _ENDIF			    ; ENDIF
     
 MONTH_LESS_THAN_CURRENT_MONTH:
-    mov	    edx, OFFSET youAreMsg
-    call    WriteString
+	    mov	    edx, OFFSET youAreMsg
+	    call    WriteString
     
-    mov	    eax, age
-    call    WriteDec
+	    mov	    eax, age
+	    call    WriteDec
 
-    mov	    edx, OFFSET yearsOldMsg
-    call    WriteString
+	    mov	    edx, OFFSET yearsOldMsg
+	    call    WriteString
 
-    ret
+	    jmp	    _ENDIF			    ; ENDIF
 
 INNER_ELSE_3:
-    ; this is a hard jump
-    mov	    edx, OFFSET dayMsg
-    call    WriteString
+	    ; this is a hard jump
+	    mov	    edx, OFFSET dayMsg
+	    call    WriteString
 
-    call    ReadDec
-    mov	    day, EAX
+	    call    ReadDec
+	    mov	    day, EAX
 
-    ;;eax=day
-    cmp	    eax, curDay
-    jg	    DAY_IS_GREATER_THAN_CURRENT_DAY ; day>curDay
-    jl	    INNER_ELSE_4		    ; day<curDay
-    je	    IS_BIRTHDAY_TODAY
+	    ;;eax=day
+	    cmp	    eax, curDay
+	    jg	    DAY_IS_GREATER_THAN_CURRENT_DAY ; IF day < curDay
+	    jl	    INNER_ELSE_4		    ; IF day > curDay
+	    je	    IS_BIRTHDAY_TODAY		    ; THEN
     
 
 DAY_IS_GREATER_THAN_CURRENT_DAY:
-    call    decrementAge
+	    call    decrementAge
 
-    mov	    edx, OFFSET youAreMsg
-    call    WriteString
+	    mov	    edx, OFFSET youAreMsg
+	    call    WriteString
     
-    mov	    eax, age
-    call    WriteDec
+	    mov	    eax, age
+	    call    WriteDec
 
-    mov	    edx, OFFSET yearsOldMsg
-    call    WriteString
+	    mov	    edx, OFFSET yearsOldMsg	    ; Print "you are "; age ; " years old "
+	    call    WriteString
 
-    ret
+	    jmp	    _ENDIF			    ; ENDIF
 
 INNER_ELSE_4:
-    mov	    edx, OFFSET youAreMsg
-    call    WriteString
+	    mov	    edx, OFFSET youAreMsg
+	    call    WriteString
     
-    mov	    eax, age
-    call    WriteDec
+	    mov	    eax, age
+	    call    WriteDec
 
-    mov	    edx, OFFSET yearsOldMsg
-    call    WriteString
+	    mov	    edx, OFFSET yearsOldMsg	    ; Print "you are "; age ; " years old "
+	    call    WriteString
 
-    ret
+	    jmp	    _ENDIF			    ; ENDIF
     
 
 IF_CURRENT_YEAR:
-    mov	    edx, OFFSET lessThanOne
-    call    WriteString
-    ret
+	    mov	    edx, OFFSET lessThanOne
+	    call    WriteString
+	    jmp	    _ENDIF
 
 IS_BIRTHDAY_TODAY:
-    mov	    edx, OFFSET	youAreMsg
-    call    WriteString
+	    mov	    edx, OFFSET	youAreMsg
+	    call    WriteString
 
-    mov	    EAX, age
-    call    WriteDec
+	    mov	    EAX, age
+	    call    WriteDec
 
-    mov	    edx, OFFSET	yearsOldMsg
-    call    WriteString
+	    mov	    edx, OFFSET	yearsOldMsg	    ; Print "you are "; age ; " years old "
+	    call    WriteString
 
-    mov	    edx, OFFSET	birthday
-    call    WriteString
+	    mov	    edx, OFFSET	birthday
+	    call    WriteString			   ; "you are "; age ;" years old, Happy Birthday"
 
-    ret
+	    jmp	    _ENDIF			   ; ENDIF
 
 PERSON_HAS_NOT_BEEN_BORN_YET:
-    mov	    edx, OFFSET notBornYet
-    call    WriteString
-    ret
+	    mov	    edx, OFFSET notBornYet
+	    call    WriteString
+	    jmp	    _ENDIF			   ; ENDIF
+
+_ENDIF:	    call    CRLF
     
 
 invoke  ExitProcess,0 ; stop
 main	    ENDP
 
 decrementAge	    PROC
-    mov	    eax, age
-    sub	    eax, 1
-    mov	    age, eax
-    ret
+		    mov	    eax, age		    ; age=age-1
+		    sub	    eax, 1
+		    mov	    age, eax
+		    ret
 decrementAge	    ENDP
 
+
 incrementAge	    PROC
-    mov	    eax, age
-    sub	    eax, 1
-    mov	    age, eax
-    ret
+		    mov	    eax, age		    ; age=age+1
+		    sub	    eax, 1
+		    mov	    age, eax
+		    ret
 incrementAge	    ENDP
 
 subYearFromCurYear  PROC
-    ;age=curYear-year
-    mov	    eax, curYear
-    sub	    eax, year
-    mov	    age, eax
-    ret
+		    ;age=curYear-year
+		    mov	    eax, curYear
+		    sub	    eax, year
+		    mov	    age, eax
+		    ret
 subYearFromCurYear ENDP
 
 END main
